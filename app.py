af5c0da2673550809ea7e69856ed8147e0b23f9e
Shahid Riaz <shahidbnriaz@gmail.com>
2024-06-28 01:19:35 -0700

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-pip install streamlit scikit-learn
 import streamlit as st
 from sklearn.model_selection import train_test_split
 
-st.write("Hello world!")
+st.title("Hello, Streamlit!")
+st.write("If you see this message, Streamlit and scikit-learn are installed correctly!")
+
+# Simple example to use train_test_split from scikit-learn
+X = [[1, 2], [3, 4], [5, 
